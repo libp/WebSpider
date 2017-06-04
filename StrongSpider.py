e9f934a4e223f26d2fa857ea3d30db455bb3856c
lbp <924701358@qq.com>
2017-06-04 10:53:28 +0800

    好尴尬，本想批量向阿杰的message模块发送消息的，最后发现他用的是node.js处理req请求，死活解析不了我传过去的json数据

--- StrongSpider.py
+++ StrongSpider.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*- 
 __author__ = 'Peng'
+from bs4 import BeautifulSoup
+
+
+
+soup = BeautifulSoup("http://tech.sina.com.cn/d/s/2017-06-03/doc-ifyfuzny2810237.shtml")
+
+
+
+print soup.prettify()
