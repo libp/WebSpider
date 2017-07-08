83c34ec4fb94140c38087e023381e242aac9bcee
lbp <924701358@qq.com>
2017-07-08 18:36:19 +0800

    多线程高并发下载图片

---
+++ StrongSpiderv2.0.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*- 
+__author__ = 'Peng'
+from bs4 import BeautifulSoup,Comment
+import urllib2
+from urllib2 import urlopen,HTTPError
+import MySQLdb
+import json
+import datetime
+import logging
+import sys
+import re
+import time
+import random
+import ConfigParser
+import threading
+import threading
+import Queue
+import time
+
+#获取infinity所有壁纸
+
+# 配置日志信息 输出到控制台
+logging.basicConfig(level=logging.INFO,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                stream=sys.stdout)
+
+def getInfinity(i):
+
+    #创建请求头
+    headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 "
+                          "(KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36",
+             "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+             "Accept-Language":"zh-CN,zh;q=0.8,en;q=0.6",
+             "Referer":"https://www.baidu.com/s?ie=UTF-8&wd=sina"}
+
+    try:
+        url="https://img.infinitynewtab.com/wallpaper/"+str(i)+".jpg"
+        request = urllib2.Request(url,headers=headers)
+        html = urlopen(request)
+        data = html.read()
+        imgName = str(i)+".jpg"
+        f = open("D:\\infinity\\"+imgName, 'wb')
+        f.write(data)
+        print u"正在保存的图片为",imgName
+        f.close()
+    except HTTPError as e:
+        print(e)
+
+
+
+
+class worker(threading.Thread):
+    def __init__(self,id, name):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.name = name
+        self.thread_stop = False
+
+    def run(self):
+        while not self.thread_stop:
+            print("thread%d %s: waiting for task" %(self.ident,self.name))
+            getInfinity(self.id)
+            self.thread_stop = True
+
+    def stop(self):
+        self.thread_stop = True
+
+
+def createThread(id,Tnum):
+    threadList = []
+    for i in range(1,Tnum):
+        threadList.append(str(i))
+
+    threads = []
+    # 创建新线程
+    for tName in threadList:
+        thread = worker(id, tName)
+        thread.start()
+        threads.append(thread)
+        id += 1
+    # 等待所有线程完成
+    for t in threads:
+        t.join()
+
+def startDownload(l,Tnum):
+    for i in range(1,l,Tnum):
+        createThread(i,Tnum)
+
+startDownload(10000,100)
---
+++ TestFile/TestThread.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*- 
+__author__ = 'Peng'
+import threading
+import Queue
+import time
+
+
+class worker(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.thread_stop = False
+
+    def run(self):
+        while not self.thread_stop:
+            print("thread%d %s: waiting for tast" % (self.ident, self.name))
+            try:
+                task = q.get(block=True, timeout=20)  # 接收消息
+            except Queue.Empty:
+                print("Nothing to do!i will go home!")
+                self.thread_stop = True
+                break
+            print("task recv:%s ,task No:%d" % (task[0], task[1]))
+            print("i am working")
+            time.sleep(3)
+            print("work finished!")
+            q.task_done()  # 完成一个任务
+            res = q.qsize()  # 判断消息队列大小
+            if res > 0:
+                print("fuck!There are still %d tasks to do" % (res))
+
+    def stop(self):
+        self.thread_stop = True
+
+
+if __name__ == "__main__":
+    q = Queue.Queue(3)
+    worker = worker(q)
+    worker.start()
+    q.put(["produce one cup!", 1], block=True, timeout=None)  # 产生任务消息
+    q.put(["produce one desk!", 2], block=True, timeout=None)
+    q.put(["produce one apple!", 3], block=True, timeout=None)
+    q.put(["produce one banana!", 4], block=True, timeout=None)
+    q.put(["produce one bag!", 5], block=True, timeout=None)
+    print("***************leader:wait for finish!")
+    q.join()  # 等待所有任务完成
+    print("***************leader:all task finished!")
